0d1284aec7a08badd37658f0fab99aeb787dfe07
jihye020201 <127707635+jihye020201@users.noreply.github.com>
2023-06-12 22:51:02 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+$(function( ){
+
+	$('#fullpage').fullpage({
+		//options here
+		autoScrolling:true,
+		scrollHorizontally: true,
+        /*navigation 현재 위치 표시 (이동 가능)
+        navigation:true,
+        navigationPosition:'right'*/
+	});
+});
